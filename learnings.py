57de897e9bb094d31228ae61399b2ad21a5d341d
sumitM-263 <hsumit263@gmail.com>
2024-12-04 19:24:04 +0530

    Update learnings.py

--- learnings.py
+++ learnings.py
@@ -3,3 +3,5 @@
 
 new_row=' '.join(str(1+i) for i in range(4))
 
+# str(i+1) for i in range(4) => Gives a list of strings i.e. ['1','2','3','4']
+# .join() functions joins each element by a space or empty string
